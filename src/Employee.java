e219e1217f10b4797a9b056a8a8d0a84ad2e8ffb
NaveenKumar-Fullstack <naveenkumargir744@gmail.com>
2025-08-14 13:21:59 +0530

    Initial commit:Set up EmployeeInfoProject with Employe class

---
+++ src/Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+    public static void main(String[] args){
+        String name;
+        int employee_ID;
+        double salary;
+
+        name="John Doe";
+        employee_ID=1001;
+        salary=75000.0;
+        System.out.println("name: "+name+"employee_ID: "+employee_ID+"salary: "+ salary);
+
+    }
+}
